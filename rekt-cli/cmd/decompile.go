972425842c433b2584f75ff4287660dc32e60d52
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2023-10-27 12:52:53 +0200

    (cli): Add decompilation support for iOS

--- rekt-cli/cmd/decompile.go
+++ rekt-cli/cmd/decompile.go
@@ -15,10 +15,10 @@ const (
 )
 
 type DecompileCmd struct {
-	name      string
-	verbose   bool
-	outputDir string
-	inputApk  string
+	name         string
+	verbose      bool
+	outputDir    string
+	inputArchive string
 }
 
 func NewDecompileCommand(flags []string) *DecompileCmd {
@@ -29,7 +29,7 @@ func NewDecompileCommand(flags []string) *DecompileCmd {
 	fs := flag.NewFlagSet(cmd.name, flag.ExitOnError)
 	fs.BoolVar(&cmd.verbose, "verbose", false, "Verbose output")
 	fs.StringVar(&cmd.outputDir, "outputDir", "", "Output directory where the decompiled app will be written to")
-	fs.StringVar(&cmd.inputApk, "apk", "", "Input APK to decompile. Required.")
+	fs.StringVar(&cmd.inputArchive, "archive", "", "Input archive to decompile. Required.")
 
 	err := fs.Parse(flags)
 	if err != nil {
@@ -46,8 +46,8 @@ func (cmd *DecompileCmd) Name() string {
 
 // Decompiles the app and returns the output directory
 func (cmd *DecompileCmd) Run() any {
-	if cmd.inputApk == "" {
-		fmt.Println("APK must be passed: rekt decompile -apk=<path-to-apk>")
+	if cmd.inputArchive == "" {
+		fmt.Println("An input archive must be passed: rekt decompile -archive=<path-to-app-archive>")
 		os.Exit(1)
 	}
 
@@ -55,25 +55,65 @@ func (cmd *DecompileCmd) Run() any {
 		PrintAscii()
 	}
 
-	baseName := strings.ReplaceAll(strings.TrimSuffix(filepath.Base(cmd.inputApk), ".apk"), " ", "_")
+	var outDir string
+
+	if strings.HasSuffix(cmd.inputArchive, ".apk") {
+		outDir = decompileAndroid(cmd)
+	} else if strings.HasSuffix(cmd.inputArchive, ".ipa") {
+		outDir = decompileIOS(cmd)
+	} else {
+		fmt.Println("Unsupported archive format. Must be APK or IPA")
+		os.Exit(1)
+	}
+
+	return outDir
+}
+
+func decompileAndroid(cmd *DecompileCmd) string {
+	baseName := strings.ReplaceAll(strings.TrimSuffix(filepath.Base(cmd.inputArchive), ".apk"), " ", "_")
 	outDir := cmd.outputDir
 	if cmd.outputDir == "" {
 		outDir = path.Join("./", "scan", baseName)
 	}
 
 	fmt.Println("Decompiling APK using jadx...")
-	utils.ExecCommand("jadx", "-d", outDir, cmd.inputApk)
+	utils.ExecCommand("jadx", "-d", outDir, cmd.inputArchive)
 
 	assetDir := path.Join(outDir, "resources/assets")
-	decompileRN(assetDir)
+	decompileRN(assetDir, "android")
 
 	fmt.Println("Decompiled app. Output directory:", outDir)
-
 	return outDir
 }
 
-func decompileRN(assetDir string) {
-	reactNativeBundle := path.Join(assetDir, "index.android.bundle")
+func decompileIOS(cmd *DecompileCmd) string {
+	baseName := strings.ReplaceAll(strings.TrimSuffix(filepath.Base(cmd.inputArchive), ".ipa"), " ", "_")
+	outDir := cmd.outputDir
+	if cmd.outputDir == "" {
+		outDir = path.Join("./", "scan", baseName)
+	}
+
+	fmt.Println("Unpacking IPA...")
+	utils.Unzip(cmd.inputArchive, outDir)
+	os.RemoveAll(path.Join(outDir, "META-INF"))
+	os.RemoveAll(path.Join(outDir, "iTunesMetadata.plist"))
+
+	matches, _ := utils.HasFile(strings.Join([]string{outDir, "Payload/*.app"}, "/"))
+	decompileRN(matches[0], "ios")
+
+	fmt.Println("Decompiled app. Output directory:", outDir)
+	return outDir
+}
+
+func decompileRN(assetDir string, platform string) {
+	bundleSuffix := fmt.Sprintf("index.%s", platform)
+	var reactNativeBundle string
+	if platform == "ios" {
+		reactNativeBundle = path.Join(assetDir, "main.jsbundle")
+	} else if platform == "android" {
+		reactNativeBundle = path.Join(assetDir, fmt.Sprintf("%s.bundle", bundleSuffix))
+	}
+	fmt.Println(reactNativeBundle)
 
 	_, ok := utils.HasFile(reactNativeBundle)
 	if ok {
@@ -81,10 +121,10 @@ func decompileRN(assetDir string) {
 		isHermesBundle := strings.Contains(out, "Hermes JavaScript bytecode")
 		if isHermesBundle {
 			fmt.Println("Decompiling Hermes bundle...")
-			utils.ExecCommand("hbc-disassembler", reactNativeBundle, path.Join(assetDir, "index.android.hasm"))
-			utils.ExecCommand("hbc-decompiler", reactNativeBundle, path.Join(assetDir, "index.android.js"))
+			utils.ExecCommand("hbc-disassembler", reactNativeBundle, path.Join(assetDir, fmt.Sprintf("%s.hasm", bundleSuffix)))
+			utils.ExecCommand("hbc-decompiler", reactNativeBundle, path.Join(assetDir, fmt.Sprintf("%s.js", bundleSuffix)))
 		} else {
-			utils.CopyFile(reactNativeBundle, path.Join(assetDir, "index.android.js"))
+			utils.CopyFile(reactNativeBundle, path.Join(assetDir, fmt.Sprintf("%s.js", bundleSuffix)))
 		}
 	}
 }
